95f24c30c7bdcc3d36508b4d284c352d64dbe6e4
brianxzaviorbrown <brianxzaviorbrown@yahoo.com>
2024-06-21 17:16:32 -0600

    Create and Close Scanner

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -1,10 +1,18 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+
 public class CashRegister {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//TODO - CREATE SCANNER
+		// TODO - PROMPT FOR INPUTS
+		// NOW COMMDAND S AND IT'S SAVED
+		Scanner Keyboard = new Scanner(System.in);
+		
+		
+		
+		Keyboard.close();
 	}
 
 }

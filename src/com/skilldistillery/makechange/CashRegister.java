54c28253c973d1c62a64cb9e2241c15f3c3534e9
brianxzaviorbrown <brianxzaviorbrown@yahoo.com>
2024-06-22 12:54:13 -0600

    If/els set up

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -4,6 +4,10 @@ public class CashRegister {
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
 		
+		//double cost = 0.0;
+		//double payment = 0.0;
+		//double number = 0.0;
+		
 //User Story #1-The user is prompted asking for the price of the item.
 		System.out.println("How much is the item?");        // Question.
 		double cost = userInput.nextDouble ();              // Answer.
@@ -11,16 +15,16 @@ public class CashRegister {
 		System.out.println("How much are you paying?");     // Question.
 		double payment = userInput.nextDouble ();		    // Answer.
 //User Story #3-Display an appropriate message if the customer provided too little money.
-		System.err.println("You have provide insufficient funds.");
+		//System.err.println("You have provide insufficient funds.");
 //User Story #3a-Display an appropriate message if the customer provided the exact amount.	
-		System.out.println("Thank you for your purchase!");
+		//System.out.println("Thank you for your purchase!");
 //User Story #4-If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer. 		
-		
-	
-		
-		
-		
-		
+		if (payment > cost) 
+		{System.out.println("There's a snake in my boot.");} // place holder until formula.
+		else if (payment < cost) 
+		{System.err.println("You have provided insufficient funds.");} // This is an error.
+		else if (payment == cost) 
+		{System.out.println("Thank you for your purchase!");} // Say thank you.
 		userInput.close();
 	}
 }
@@ -55,12 +59,6 @@ public class CashRegister {
 // I need questions and get answers using a system.out.println and a scanner.
 // I need if and else if statrments for Thank you and error lines otherwise it'll pop up by default.
 // Figure out a formula for giving change back.
-//
-// What can I use? Switch, if/else, mod, typecasting,
-//
-//
-//
-//
-//
-//
+// What can I use? Switch, if/else, mod, typecasting, method,
+
 
